6a1343046d50b7a5e9abfe37a590e2ea5083098b
Cory Moultrie <cmoultrie@fullsail.com>
2021-12-08 01:15:10 -0500

    Removed 4 write.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace Learning
             Console.WriteLine("New Branch!");
             Console.WriteLine("Second commit!");
             Console.WriteLine("Branch 3!");
-            Console.WriteLine("Branch 4!");
         }
     }
 }
