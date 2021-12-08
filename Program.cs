92ab78cbe6f9aebed4c7068d43b9a1765f29c34d
Cory Moultrie <cmoultrie@fullsail.com>
2021-12-08 01:39:11 -0500

    Bye 3.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ namespace Learning
             Console.WriteLine("Hello World!");
             Console.WriteLine("New Branch!");
             Console.WriteLine("Second commit!");
-            Console.WriteLine("Branch 3!");
             Console.WriteLine("Branch 4!");
             Console.WriteLine("New 2!");
             Console.WriteLine("Main main!");
